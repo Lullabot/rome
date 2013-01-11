3a6d1ac4034fbe653aff99234c1031ccfd968e4f
Andrew Berry <andrew.berry@lullabot.com>
2013-01-11 17:16:48 -0500

    Add configuration for the MySQL server.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,15 @@
+node "mysql" {
+  include mysql::server
+
+  # Keep track of our customized configuration files.
+  #file { '/etc/mysql/my.cnf':
+  #    ensure  => present,
+  #    source  => '/vagrant/files/mysql/etc/mysql/my.cnf',
+  #    owner   => 'root',
+  #    group   => 'root',
+  #}
+}
+
 node "memcache" {
   class { 'apt':
     always_apt_update => true,
