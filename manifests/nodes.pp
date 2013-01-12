1e5e27c2c5c4f1cd5565660b1323f8463534a4e4
Andrew Berry <andrew.berry@lullabot.com>
2013-01-11 19:01:49 -0500

    Add the Apache node configuration.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -25,6 +25,49 @@ node "memcache" {
   }
 }
 
+node "apache" {
+  include base
+  include apache
+  include php
+  include mysql::client
+
+  class { 'apt':
+    always_apt_update => true,
+  }
+
+  package { 'openjdk-7-jre-headless':
+      ensure  => present,
+  }
+
+  file { [ '/usr/local/share/', '/usr/local/share/tika' ]:
+      ensure => directory,
+      owner   => 'root',
+      group   => 'root',
+  }
+
+  # Download tika so we don't have to keep it in our git repository (it's 25M).
+  download {
+    "/usr/local/share/tika/tika-app-1.2.jar":
+      uri => "http://www.apache.org/dyn/closer.cgi/tika/tika-app-1.2.jar",
+      timeout => 900
+  }
+
+  # Keep track of our customized configuration files.
+  file { '/etc/php5/apache2/php.ini':
+      ensure  => present,
+      source  => '/vagrant/files/apache/etc/php5/apache2/php.ini',
+      owner   => 'root',
+      group   => 'root',
+  }
+
+  file { '/etc/crontab':
+      ensure => present,
+      source => '/vagrant/files/apache/etc/crontab',
+      owner   => 'root',
+      group   => 'root',
+  }
+}
+
 node "solr" {
   Exec {
     path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
@@ -40,3 +83,12 @@ node "solr" {
   solr::index::drupal { 'apache.juno.local': version => '7.x-1.1' }
 }
 
+define download ($uri, $timeout = 300) {
+    exec {
+        "download $uri":
+            command => "/usr/bin/wget -q '$uri' -O $name",
+            creates => $name,
+            timeout => $timeout,
+            require => Package[ "wget" ],
+    }
+}
